7c0a297b64f659193c27c25fe0bd67ef704e1ad8
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-06-11 18:55:53 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name: string;
+  date: string;
+  amount: number;
+  height: number;
+  miles: number;
+
+  onMilesChange(value: string) {
+    this.miles = parseFloat(value);
+  }
+
+  onHeightChange(value: string) {
+    this.height = parseFloat(value);
+  }
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
+
+  onDateChange(value: string) {
+    this.date = value;
+  }
+
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
+}
